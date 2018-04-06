387026ad15cf45c9f484221a97bee04f99b1f7b0
Tom Wagner <wagne868@umn.edu>
2018-04-06 13:52:52 -0400

    Add notes within API_calls.js file

--- server/helpers/API_calls.js
+++ server/helpers/API_calls.js
@@ -1,5 +1,14 @@
-// EXAMPLE OF WORKING GET REQUEST:
-// http://api.openweathermap.org/data/2.5/forecast?zip=10029&appid=fde31744adf95ba87998914da3123556
+// ZIP CODE: http://api.wunderground.com/api/key/forecast10day/q/10029.json
+
+// USER LOCATION: http://api.wunderground.com/api/key/forecast10day/q/autoip.json
+
+// Resources: https://apigee.com/resources/wunderground?apig_cc=1
+
+// More resources: https://www.wunderground.com/weather/api/d/docs?d=index
+
+
+
+
 
 
 
